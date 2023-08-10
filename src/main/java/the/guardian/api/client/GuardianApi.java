50cceb624b396dff2b84db965655fb21dfcdb3db
Omoniyi Omotoso <omoniyiomotoso@gmail.com>
2023-08-10 02:13:13 +0200

    adding more javaDocs comments

--- src/main/java/the/guardian/api/client/GuardianApi.java
+++ src/main/java/the/guardian/api/client/GuardianApi.java
@@ -3,51 +3,82 @@ package the.guardian.api.client;
 import the.guardian.api.config.UrlMap;
 import the.guardian.api.entity.*;
 
-public class GuardianApi{
+/**
+ * Represents a client for interacting with The Guardian API.
+ */
+public class GuardianApi {
     private String apiKey;
 
+    /**
+     * Constructs a GuardianApi instance with the provided API key.
+     *
+     * @param _apiKey The API key to be used for authentication.
+     * @throws IllegalArgumentException If the provided API key is too short.
+     */
     public GuardianApi(String _apiKey) {
         validateApiKey(_apiKey);
         this.apiKey = _apiKey;
     }
 
     /**
-     * make sure the api key is valid
+     * Validates whether the provided API key is valid.
+     *
+     * @param apiKey The API key to be validated.
+     * @throws IllegalArgumentException If the provided API key is too short.
      */
-    private String validateApiKey(String apiKey) {
+    private void validateApiKey(String apiKey) {
         if (apiKey.length() < 4) {
-            throw new IllegalArgumentException("API key "+ apiKey +" is too short, and thus invalid.");
+            throw new IllegalArgumentException("API key " + apiKey + " is too short, and thus invalid.");
         }
-
-        return apiKey;
     }
-    
+
+    /**
+     * Returns a Content instance for interacting with content-related operations of the API.
+     *
+     * @return A Content instance.
+     */
     public Content content() {
-        String apiUrl =  new UrlMap().CONTENT + "?api-key=" + this.apiKey;
+        String apiUrl = new UrlMap().CONTENT + "?api-key=" + this.apiKey;
         return new Content(apiUrl);
     }
 
-    
+    /**
+     * Returns a Tags instance for interacting with tags-related operations of the API.
+     *
+     * @return A Tags instance.
+     */
     public Tags tags() {
-        String apiUrl =  new UrlMap().TAGS + "?api-key=" + this.apiKey;
+        String apiUrl = new UrlMap().TAGS + "?api-key=" + this.apiKey;
         return new Tags(apiUrl);
     }
 
-    
+    /**
+     * Returns a Sections instance for interacting with sections-related operations of the API.
+     *
+     * @return A Sections instance.
+     */
     public Sections sections() {
-        String apiUrl =  new UrlMap().SECTIONS+ "?api-key=" + this.apiKey;
+        String apiUrl = new UrlMap().SECTIONS + "?api-key=" + this.apiKey;
         return new Sections(apiUrl);
     }
 
-    
+    /**
+     * Returns an Editions instance for interacting with editions-related operations of the API.
+     *
+     * @return An Editions instance.
+     */
     public Editions editions() {
-        String apiUrl =  new UrlMap().EDITIONS + "?api-key=" + this.apiKey;
+        String apiUrl = new UrlMap().EDITIONS + "?api-key=" + this.apiKey;
         return new Editions(apiUrl);
     }
 
-    
+    /**
+     * Returns a SingleItem instance for interacting with single item-related operations of the API.
+     *
+     * @return A SingleItem instance.
+     */
     public SingleItem singleItem() {
-        String apiUrl =  new UrlMap().BASE;
+        String apiUrl = new UrlMap().BASE;
         return new SingleItem(apiUrl, this.apiKey);
     }
 }
--- src/main/java/the/guardian/api/entity/ApiEntity.java
+++ src/main/java/the/guardian/api/entity/ApiEntity.java
@@ -11,50 +11,88 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 /**
- * Base class for all available guardian API entities
+ * Abstract base class for entities interacting with The Guardian API.
  */
-
 public abstract class ApiEntity {
     protected String query = "";
-
     protected String baseUrl = "";
-
     protected String format = "";
-    
+
+    /**
+     * Retrieves the base URL for API requests.
+     *
+     * @return The base URL.
+     */
     public String getBaseUrl() {
         return this.baseUrl;
     }
 
+    /**
+     * Sets the base URL for API requests.
+     *
+     * @param data The base URL.
+     */
     public void setBaseUrl(String data) {
         this.baseUrl = data;
     }
 
-    
+    /**
+     * Retrieves the format for API responses.
+     *
+     * @return The response format.
+     */
     public String getFormat() {
         return this.format;
     }
 
+    /**
+     * Sets the format for API responses.
+     *
+     * @param data The response format.
+     */
     public void setFormat(String data) {
         this.format = data;
     }
 
-    
-    public ApiEntity appendToBaseUrl(String attributeName,  String attributeValue) throws UnsupportedEncodingException {
+    /**
+     * Appends an attribute to the base URL.
+     *
+     * @param attributeName  The name of the attribute to append.
+     * @param attributeValue The value of the attribute to append.
+     * @return The updated ApiEntity instance.
+     * @throws UnsupportedEncodingException If encoding the attribute value fails.
+     */
+    public ApiEntity appendToBaseUrl(String attributeName, String attributeValue) throws UnsupportedEncodingException {
         if (!attributeValue.equals("")) {
             String encodedValue = URLEncoder.encode(attributeValue, "UTF-8");
             String url = "&" + attributeName + "=" + encodedValue;
-            String var10001 = this.baseUrl;
-            this.baseUrl = var10001 + url;
+            this.baseUrl += url;
         }
-
         return this;
     }
 
+    /**
+     * Builds the complete URL for API requests.
+     *
+     * @return The complete URL.
+     * @throws UnsupportedEncodingException If encoding the URL components fails.
+     */
     public abstract String buildUrl() throws UnsupportedEncodingException;
 
-    
+    /**
+     * Fetches data from the API and returns the response.
+     *
+     * @return The API response as an AbstractResponse object.
+     * @throws UnsupportedEncodingException If encoding the URL components fails.
+     * @throws UnirestException            If a Unirest HTTP request exception occurs.
+     */
     public abstract AbstractResponse fetch() throws UnsupportedEncodingException, UnirestException;
 
+    /**
+     * Constructs an ApiEntity instance with the provided base URL.
+     *
+     * @param _baseUrl The base URL for API requests.
+     */
     public ApiEntity(String _baseUrl) {
         super();
         this.format = "json";
@@ -63,12 +101,13 @@ public abstract class ApiEntity {
 
     static {
         Unirest.setObjectMapper(new ObjectMapper() {
-            private final com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper
-                    = new com.fasterxml.jackson.databind.ObjectMapper();
+            private final com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper =
+                    new com.fasterxml.jackson.databind.ObjectMapper();
 
             public <T> T readValue(String value, Class<T> valueType) {
                 try {
-                    return jacksonObjectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES).readValue(value, valueType);
+                    return jacksonObjectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                            .readValue(value, valueType);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
